9c3616a63ee4ba413458b25d487eed68ab34ef5d
quocanh1010 <82823336+quocanh1010@users.noreply.github.com>
2021-08-20 16:51:08 +0700

    h

--- Project cuối kì/ProjectWeb16 4/conponent/banner/index.js
+++ Project cuối kì/ProjectWeb16 4/conponent/banner/index.js
@@ -1,35 +1,41 @@
 let banner = $('#banner')
 
 banner.append(`
-<div class="main-banner">
-<div class="left-banner"><div>Tin Nóng</div>
-    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
-    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
-    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
-    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
-    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
+<body><div class="container">
+<div class="row">
+    <div class="col-sm-3 col-md-2">
+<div class="left-banner"><div style="color: red; font-size: 24px;">Tin Nóng</div>
+    <div style="background-color: #e5e5e5;">Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
+    <div style="background-color: #c4c4c4;">Người đại diện gõ cửa, Man Utd tiến gần chữ ký thứ 4</div>
+    <div style="background-color: #e5e5e5;">Xác nhận đối thủ đề nghị ký HĐ, Arsenal nguy cơ mất chữ ký chất lượng</div>
+    <div style="background-color:#c4c4c4;">Arsenal đưa kẻ thừa PSG vào tầm ngắm</div>
+    <div style="background-color: #e5e5e5;">Silva và Jorginho khiến Tuchel phá lên cười vì 1 điều đặc biệt</div></div>
 
 </div>
+<div class="col-sm-9 col-md-8">
 <div class="center-banner">
     <div class="center-banner-top">
         <img src="./img/Rectangle 41.png" alt="">
-        <div>Hủy diệt Everton, Man Utd tình cờ phát hiện bộ đôi tiền vệ trong mơ</div>
+        <div style="text-transform: uppercase; font-weight:bold; font-size:20px">Hủy diệt Everton, Man Utd tình cờ phát hiện bộ đôi tiền vệ trong mơ</div>
         <div>Man United vừa có thắng lợi 4-0 ấn tượng trước Everton trong trận giao hữu cuối cùng của mùa hè 2021.</div>
     </div>
     <div class="center-banner-bottom">
+        <div style="width:50%">
         <img src="./img/Rectangle 49.png" alt="">
-        <div>10 sao cày ải nhiều trận nhất mùa 2020/21: Pedri số 1, sao Chelsea số 3</div>
-        <img src="./img/Rectangle 46.png" alt="">
-        <div>Ra mắt Man City, Grealish đã thể hiện như thế nào?</div>
+        <div>10 sao cày ải nhiều trận nhất mùa 2020/21: Pedri số 1, sao Chelsea số 3</div></div>
+        <div  style="width:50%"><img src="./img/Rectangle 46.png" alt="">
+        <div>Ra mắt Man City, Grealish đã thể hiện như thế nào?</div></div>
     </div>
-</div>
-<div class="right-banner"><div>Tin Đọc Nhiều</div>
-    <img src="./img/Rectangle 47.png" alt="">
-    <div>M.U sẵn sàng bán đứt cầu thủ 28 tuổi, từ chối cho mượn</div>
-    <img src="./img/Rectangle 50.png" alt="">
-    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
-    <img src="./img/Rectangle 51.png" alt="">
-    <div>CĐV Man Utd: "Eric Ramsay là bản hợp đồng của mùa giải"</div>
+</div></div>
+<div class="col-sm-12 col-md-2">
+<div class="right-banner"><div style="color: red; font-size: 24px;"  class="right-banner-items">Tin Đọc Nhiều</div>
+    <div class="right-banner-items"><img src="./img/Rectangle 47.png" alt="">
+    <div>M.U sẵn sàng bán đứt cầu thủ 28 tuổi, từ chối cho mượn</div></div>
+    <div class="right-banner-items"><img src="./img/Rectangle 50.png" alt="">
+    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div></div>
+    <div  class="right-banner-items" ><img src="./img/Rectangle 51.png" alt="">
+    <div>CĐV Man Utd: "Eric Ramsay là bản hợp đồng của mùa giải"</div></div>
+</div></div>
 </div>
 </div>
 `)
