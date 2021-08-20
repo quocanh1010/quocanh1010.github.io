ab0458a293c0f37fe83dd687b38949e84025147c
quocanh1010 <82823336+quocanh1010@users.noreply.github.com>
2021-08-20 14:01:09 +0700

    gg

--- Project cuối kì/ProjectWeb16 4/conponent/topbar/index.js
+++ Project cuối kì/ProjectWeb16 4/conponent/topbar/index.js
@@ -1,15 +1,15 @@
-let banner = $('#banner')
+let topbar = $('#topbar')
 
-banner.append(`
-<div class="banner">
-<div class="banner-left">
+topbar.append(`
+<div class="topbar">
+<div class="topbar-left">
     <b style="color: red; text-decoration: underline;" class="text">MỚI NHẤT :
     </b>
     <div>Đội tuyển Việt Nam sẽ gặp nhiều thách thức trong SEAGames</div>
 </div>
 
-<div class="banner-right">
-    <ul class="banner-right-items">
+<div class="topbar-right">
+    <ul class="topbar-right-items">
         <li>Arsenal</li>
         <li>Chelsea</li>
         <li>MU</li>
