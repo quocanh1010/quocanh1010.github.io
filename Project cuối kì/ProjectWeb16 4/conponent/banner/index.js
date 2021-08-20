b7bf4a0e3fffe9886ae64df778b87e54ab9b520c
quocanh1010 <82823336+quocanh1010@users.noreply.github.com>
2021-08-20 13:05:21 +0700

    d

--- Project cuối kì/ProjectWeb16 4/conponent/banner/index.js
+++ Project cuối kì/ProjectWeb16 4/conponent/banner/index.js
@@ -1,19 +1,15 @@
 let banner = $('#banner')
 
 banner.append(`
-<div class="topbar">
-<div class="topbar-left">
+<div class="banner">
+<div class="banner-left">
     <div class="text">MỚI NHẤT :
     </div>
     <div>Đội tuyển Việt Nam sẽ gặp nhiều thách thức trong SEAGames</div>
 </div>
 
-<div class="topbar-right">
-    <div>Arsenal</div>
-    <img src="./img/Line 5 (Stroke).png" alt="">
-    <div>Chelsea</div>
-    <img src="./img/Line 5 (Stroke).png" alt="">
-    <div>MU</div>
+<div class="banner-right">
+    <ul c
 </div>
 </div>
 
