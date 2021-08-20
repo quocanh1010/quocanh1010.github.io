0419e92c7c2d2e6b9d1dd4fc5b375606accd7b7d
quocanh1010 <82823336+quocanh1010@users.noreply.github.com>
2021-08-20 15:35:26 +0700

    g

---
+++ Project cuối kì/ProjectWeb16 4/conponent/banner/index.js
@@ -0,0 +1,35 @@
+let banner = $('#banner')
+
+banner.append(`
+<div class="main-banner">
+<div class="left-banner"><div>Tin Nóng</div>
+    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
+    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
+    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
+    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
+    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
+
+</div>
+<div class="center-banner">
+    <div class="center-banner-top">
+        <img src="./img/Rectangle 41.png" alt="">
+        <div>Hủy diệt Everton, Man Utd tình cờ phát hiện bộ đôi tiền vệ trong mơ</div>
+        <div>Man United vừa có thắng lợi 4-0 ấn tượng trước Everton trong trận giao hữu cuối cùng của mùa hè 2021.</div>
+    </div>
+    <div class="center-banner-bottom">
+        <img src="./img/Rectangle 49.png" alt="">
+        <div>10 sao cày ải nhiều trận nhất mùa 2020/21: Pedri số 1, sao Chelsea số 3</div>
+        <img src="./img/Rectangle 46.png" alt="">
+        <div>Ra mắt Man City, Grealish đã thể hiện như thế nào?</div>
+    </div>
+</div>
+<div class="right-banner"><div>Tin Đọc Nhiều</div>
+    <img src="./img/Rectangle 47.png" alt="">
+    <div>M.U sẵn sàng bán đứt cầu thủ 28 tuổi, từ chối cho mượn</div>
+    <img src="./img/Rectangle 50.png" alt="">
+    <div>Chelsea kiểm tra y tế, ra mắt bản HĐ đắt kỷ lục trong vòng 24h</div>
+    <img src="./img/Rectangle 51.png" alt="">
+    <div>CĐV Man Utd: "Eric Ramsay là bản hợp đồng của mùa giải"</div>
+</div>
+</div>
+`)
